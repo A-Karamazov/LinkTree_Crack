6b491b19db6724f6ea6f7c3e810316028223d0b6
A-Karamazov <kabecudinho@gmail.com>
2026-08-12 09:50:06 -0300

    Update class.usuario.ts

--- src/class.usuario.ts
+++ src/class.usuario.ts
@@ -1,13 +1,26 @@
 import { db } from "./connnection"
 
 class Usuario {
-    constructor (
-    private id: number,
-    private usuario: string,
-    private grupos: string
-    ) {}
+    constructor(
+        private id: number,
+        private usuario: string,
+        private senha: string,
+        private grupos: string
+    ) { }
 
     salvar() {
-        
+        const q = db.query(`
+            INSERT INTO usuarios(usuario, senha, grupos)
+                VALUES($usuario, $senha, $grupos)
+        `)
+
+        const response = q.run({
+            $usuario: this.usuario,
+            $senha: this.senha,
+            $grupos: this.grupos
+        })
     }
 }
+
+const u1 = new Usuario(0, "carlinhos", "qwerty", "fa de cavalos")
+u1.salvar()
